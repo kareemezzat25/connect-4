e28f771207c1be5aa59c700ca7c2ce238e38cba9
kareem_ezzat <117308291+kareemezzat25@users.noreply.github.com>
2023-05-19 15:24:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,128 +1,93 @@
-import sys
-import numpy as np
-import pygame
-import math
-# board rows=6 columns=7
-red = (255, 0, 0)
-blue = (0, 0, 0)
-yellow = (255, 255, 0)
-white = (255, 255, 255)
 num_rows = 6
 num_columns = 7
-def create_board():
-    board = np.zeros((num_rows,num_columns))
-    return  board
-def drop_piece(board,row,column,piece):
-    board[row][column] = piece
-# top row in column not full
-def isvalid_location(board,column):
-    # 5
-    return board[5][column] == 0
-
-def next_emptyrow(board, column):
-    for row in range(6):
+player_piece = 1
+Ai_piece = 2
+# check if top row is empty or not
+def isValid_location(board, column):
+    return board[num_rows-1][column] == 0
+# get next empty row for this column
+def getNext_emptyRow(board,column):
+    for row in range(num_rows):
         if board[row][column] == 0:
             return row
-def wining_move(board,piece):
-    # check horizotal
-    for col in range (num_columns-3):
+# function who is win
+def winning(board,piece):
+    # check horizontal
+    for column in range(num_columns-3):
         for row in range(num_rows):
-            if board[row][col] == piece and board[row][col+1] == piece and board[row][col+2] == piece and board[row][col+3] == piece :
+            # - - - -
+            if board[row][column] == piece and board[row][column+1] == piece and board[row][column+2] == piece and board[row][column+3] == piece:
                 return True
     # check vertical
-    for col in range(num_columns):
+    for column in range(num_columns):
         for row in range(num_rows-3):
-            if board[row][col] == piece and board[row+1][col]== piece and board[row+2][col] == piece and board [row+3][col] == piece:
+            # check if 4 row in same column have same piece or no
+            if board[row][column] == piece and board[row+1][column] == piece and board[row+2][column] == piece and board[row+3][column] == piece:
                 return True
-        # diagonal
-    for col in range(num_columns-3):
+      # check diagonal
+    for column in range(num_columns-3):
         for row in range(num_rows-3):
-            if board[row][col] == piece and board[row + 1][col+1] == piece and board[row + 2][col+2] == piece and board[row + 3][col+3] == piece:
+            if board[row][column] == piece and board[row+1][column+1] == piece and board[row+2][column+2] == piece and board[row+3][column+3] == piece:
                 return True
-        # negative diagonal
-    for col in range(num_columns - 3):
+
+    for column in range(num_columns-3):
         for row in range(3, num_rows):
-            if board[row][col] == piece and board[row - 1][col + 1] == piece and board[row - 2][col + 2] == piece and board[row - 3][col + 3] == piece:
+            if board[row][column] == piece and board[row-1][column+1] == piece and board[row-2][column+2] == piece and board[row-3][column+3] == piece:
                 return True
+def evaluate_window(window,piece):
+    score = 0
+    opp_piece = player_piece
+    # if piece == 2 else 2
+    if window.count(piece) == 4:
+        score += 100
+    elif window.count(piece) == 3 and window.count(0) == 1:
+        score += 5
+    elif window.count(piece) == 2 and window.count(0) == 2:
+        score +=2
+    # check in opposite piece
+    if window.count(opp_piece) == 3 and window.count(0) == 1:
+        score -=4
+    return score
 
-def print_flipboard( board ):
-    print(np.flip(board, 0))
-def print_boardGraphics(board):
-    for c in range(num_columns):
-        for r in range(num_rows):
-            # rectangle column, row height width
-            # shift with row
-            pygame.draw.rect(screen, yellow, (c*squareSize, r*squareSize+squareSize, squareSize, squareSize))
-            pygame.draw.circle(screen, white, (int(c*squareSize+squareSize/2), int(r*squareSize+squareSize+squareSize/2)), radius)
-    for c in range(num_columns):
-        for r in range(num_rows):
-            # player number 1 ------>1 (red)
-            if board[r][c] == 1:
-                pygame.draw.circle(screen, red, (int(c * squareSize + squareSize / 2), height-int(r * squareSize + squareSize / 2)), radius)
-            # player number 2 ------->2 (blue)
-            elif board[r][c] == 2 :
-                pygame.draw.circle(screen, blue, (int(c * squareSize + squareSize / 2), height-int(r * squareSize + squareSize / 2)), radius)
-    pygame.display.update()
-board = create_board()
-game_over = False
-pygame.init()
-# width and height of this game
-squareSize = 100
-width = num_columns*squareSize
-height = (num_rows+1)*squareSize
-size = (width, height)
-# radius of circle
-radius = int(squareSize/2 - 5)
-screen = pygame.display.set_mode(size)
-print_boardGraphics(board)
-pygame.display.update()
-# to write who is win
-text = pygame.font.SysFont("monospace", 65)
-# game not over
-Flag = 0
-while not game_over:
-    for event in pygame.event.get():
-       # if event.type == pygame.quit():
-         #   sys.exit()
-       if event.type == pygame.MOUSEMOTION:
-           position=event.pos[0]
-           pygame.draw.rect(screen, white, (0,0, width, squareSize))
-           if Flag == 0:
-               # player 1
-               pygame.draw.circle(screen, red, (position, int(squareSize/2)), radius)
-           else:
-               pygame.draw.circle(screen, blue, (position, int(squareSize / 2)), radius)
+def position_ofScore(board, piece):
+    score = 0
+    centerArray = [int(i) for i in list(board[:, 3])]
+    centerCount = centerArray.count(piece)
+    score += centerCount*3
+    for row in range (board.shape[0]):
+        rowArray = [int(i) for i in list(board[row, :])]
+        for column in range(num_columns-3):
+            window = rowArray[column:column+4]
+            score += evaluate_window(window,piece)
+    # score of vertical
+    for column in range (num_columns):
+        columnArray = [int(i) for i in list(board[:,column])]
+        for row in range(num_rows-3):
+            window = columnArray[row:row+4]
+            score += evaluate_window(window,piece)
+    # score diagonal
+    for row in range(num_rows-3):
+        for column in range(num_columns-3):
+            window = [board[row+i][column+i] for i in range(4)]
+            score += evaluate_window(window, piece)
 
-       pygame.display.update()
-        # drop piece
-       if event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.rect(screen, white, (0, 0, width, squareSize))
-            # input_of_player1
-            if Flag == 0:
-                # pos
-                posplayer1 = event.pos[0]
-                columnPlayer1 = int(math.floor(posplayer1/squareSize))
-                if isvalid_location(board, columnPlayer1):
-                    row = next_emptyrow(board, columnPlayer1)
-                    drop_piece(board, row, columnPlayer1, 1)
-                    if wining_move(board, 1) == True:
-                        label = text.render("Player 1 win !!!!", 1, red)
-                        screen.blit(label, (10, 10))
-                        game_over = True
-                    Flag = 1
-            else:
-                posplayer2 = event.pos[0]
-                columnPlayer2 = int(math.floor(posplayer2 / squareSize))
-                if isvalid_location(board, columnPlayer2):
-                    row = next_emptyrow(board, columnPlayer2)
-                    drop_piece(board, row, columnPlayer2, 2)
-                    if wining_move(board, 2) == True:
-                       # print_flipboard(board)
-                       label = text.render("Player 2 win !!!!", 1,blue)
-                       screen.blit(label, (10, 10))
-                       game_over = True
-                    Flag = 0
-            print_flipboard(board)
-            print_boardGraphics(board)
-            if game_over:
-                pygame.time.wait(4000)
+    for row in range(num_rows - 3):
+        for column in range(num_columns - 3):
+            window = [board[row +3- i][column + i] for i in range(4)]
+            score += evaluate_window(window, piece)
+    return score
+# return list of columns that have valid locations
+def getValidLocations(board):
+    validLocations = []
+    for column in range(num_columns):
+        if isValid_location(board, column):
+            validLocations.append(column)
+    return validLocations
+# check if this is terminalnode or not
+def isTerminal(board):
+    # one of player win or no place to add in it
+    return winning(board, player_piece) or winning(board, Ai_piece) or len((getValidLocations(board))) == 0
+# minimax
+def minimax(board,depth,alpha,beta,maximizing_player):
+    # get the columns that have valid locations
+    validlocations = getValidLocations()
